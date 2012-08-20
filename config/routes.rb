893f945c300dd1d0b66fdc19d87e6e15597fe412
pradeepreddyk <pradeep.reddy.iitm@gmail.com>
2012-08-20 16:07:27 +0530

    Finish layou routes and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FirstApp::Application.routes.draw do
+ # get "users/new"
+
   root to: 'static_pages#home'
-  #match '/home',    to: 'static_pages#home'
+  match '/signup',    to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
